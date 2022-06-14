826a6922bfb40c82fe6440b81aeadb4ac9b2c670
Alexandru-Ionut.Balan <alexandru-ionut.balan@ing.com>
2022-06-14 10:36:53 +0300

    Initial commit

---
+++ src/main/java/io/simun/core/SimunCoreApplication.java
@@ -0,0 +1,13 @@
+package io.simun.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimunCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimunCoreApplication.class, args);
+    }
+
+}
